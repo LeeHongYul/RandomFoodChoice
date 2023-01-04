e9dc9c98d416262ec6aebf05bdfa62ca24459815
hongryul <ryullee0911@gmail.com>
2023-01-04 12:44:13 +0900

    Commit from Xcode

--- RandomFoodChoice/ViewController.swift
+++ RandomFoodChoice/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let FoodArray = ["김치찌개","마라탕","쌀국수","금촌","돈까스","육회비빔밥","마라샹궈","떡볶이"]
+    
+    @IBOutlet var foodName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    showFood()
+    }
+    
+    
+    func showFood(){
+        foodName.text = "메뉴 대신 골라줍니다"
+        
     }
 
-
+    @IBAction func foodBtn(_ sender: UIButton) {
+    
+        foodName.text = FoodArray.randomElement()
+    }
 }
 
